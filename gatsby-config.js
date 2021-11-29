d08081b1f6fe917d608812d2fd6b29889b4fe0b4
Benjamin Michaelis <benjamin@michaelis.net>
2021-11-29 12:46:03 -0800

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://bencodes.michaelis.net/",
+    title: "Benjamin Michaelis",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-S10Q6VG46J",
+      },
+    },
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
