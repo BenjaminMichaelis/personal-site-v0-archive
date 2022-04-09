b2cac28cd4a22959a81233fccfd190be0f805107
Benjamin Michaelis <benjamin@michaelis.net>
2022-04-08 20:32:08 -0700

    Update src/html.js

--- src/html.js
+++ src/html.js
@@ -17,7 +17,7 @@ export default function HTML(props) {
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
